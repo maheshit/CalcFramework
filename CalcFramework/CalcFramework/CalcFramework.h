fe2756ecafd1d8bb6faa409a1dc695ed583ca92b
ArjunReddy <mallikarjuna.reddy@emxcelsolutions.com>
2020-03-20 00:54:27 +0530

    Initial commit

---
+++ CalcFramework/CalcFramework/CalcFramework.h
@@ -0,0 +1,19 @@
+//
+//  CalcFramework.h
+//  CalcFramework
+//
+//  Created by Mahesh on 19/03/20.
+//  Copyright © 2020 emxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalcFramework.
+FOUNDATION_EXPORT double CalcFrameworkVersionNumber;
+
+//! Project version string for CalcFramework.
+FOUNDATION_EXPORT const unsigned char CalcFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalcFramework/PublicHeader.h>
+
+
---
+++ CalcFramework/CalcFramework/CalcView.swift
@@ -0,0 +1,21 @@
+//
+//  CalcView.swift
+//  CalcFramework
+//
+//  Created by Mahesh on 19/03/20.
+//  Copyright © 2020 emxc. All rights reserved.
+//
+
+import UIKit
+
+public class CalcView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
